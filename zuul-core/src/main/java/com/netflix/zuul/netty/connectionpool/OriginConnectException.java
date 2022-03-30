f569f3ad1a406553d9bd876b77ab29987deb7375
nimakarimipour <karimipour.nima@gmail.com>
2022-03-30 13:34:19 -0700

    Final Result

--- zuul-core/src/main/java/com/netflix/zuul/netty/connectionpool/OriginConnectException.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/connectionpool/OriginConnectException.java
@@ -13,10 +13,9 @@
  *      See the License for the specific language governing permissions and
  *      limitations under the License.
  */
-
 package com.netflix.zuul.netty.connectionpool;
 
-
+import javax.annotation.Nullable;
 import com.netflix.zuul.exception.ErrorType;
 
 /**
@@ -26,7 +25,7 @@ public class OriginConnectException extends Exception {
 
     private final ErrorType errorType;
 
-    public OriginConnectException(String message, ErrorType errorType) {
+    public OriginConnectException(@Nullable String message, ErrorType errorType) {
         // ensure this exception does not fill its stacktrace, this causes a 10x slowdown
         super(message, null, true, false);
         this.errorType = errorType;
@@ -41,5 +40,4 @@ public class OriginConnectException extends Exception {
     public ErrorType getErrorType() {
         return errorType;
     }
-
 }
