21d018c32245b7f7fa90c545f51c669e0e3a257c
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 03:20:49 -0700

    Final Result

--- zuul-core/src/main/java/com/netflix/netty/common/channel/config/ChannelConfigKey.java
+++ zuul-core/src/main/java/com/netflix/netty/common/channel/config/ChannelConfigKey.java
@@ -13,27 +13,28 @@
  *      See the License for the specific language governing permissions and
  *      limitations under the License.
  */
-
 package com.netflix.netty.common.channel.config;
+
+import javax.annotation.Nullable;
 
 /**
  * User: michaels@netflix.com
  * Date: 2/8/17
  * Time: 6:17 PM
  */
-public class ChannelConfigKey<T>
-{
+public class ChannelConfigKey<T> {
+
     private final String key;
+
+    @Nullable
     private final T defaultValue;
 
-    public ChannelConfigKey(String key, T defaultValue)
-    {
+    public ChannelConfigKey(String key, T defaultValue) {
         this.key = key;
         this.defaultValue = defaultValue;
     }
 
-    public ChannelConfigKey(String key)
-    {
+    public ChannelConfigKey(String key) {
         this.key = key;
         this.defaultValue = null;
     }
@@ -42,22 +43,16 @@ public class ChannelConfigKey<T>
         return key;
     }
 
-    public T defaultValue()
-    {
+    public T defaultValue() {
         return defaultValue;
     }
 
-    public boolean hasDefaultValue()
-    {
+    public boolean hasDefaultValue() {
         return defaultValue != null;
     }
 
     @Override
-    public String toString()
-    {
-        return "ChannelConfigKey{" +
-                "key='" + key + '\'' +
-                ", defaultValue=" + defaultValue +
-                '}';
+    public String toString() {
+        return "ChannelConfigKey{" + "key='" + key + '\'' + ", defaultValue=" + defaultValue + '}';
     }
 }
