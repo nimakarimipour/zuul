5d39d88e81b9d1c4baa5809bc041a985cb9ea0b5
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 03:35:43 -0700

    Final Result

--- zuul-core/src/main/java/com/netflix/zuul/netty/filter/FilterRunner.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/filter/FilterRunner.java
@@ -13,9 +13,9 @@
  *      See the License for the specific language governing permissions and
  *      limitations under the License.
  */
-
 package com.netflix.zuul.netty.filter;
 
+import javax.annotation.Nullable;
 import com.netflix.zuul.message.ZuulMessage;
 import io.netty.handler.codec.http.HttpContent;
 
@@ -24,6 +24,7 @@ import io.netty.handler.codec.http.HttpContent;
  */
 public interface FilterRunner<I extends ZuulMessage, O extends ZuulMessage> {
 
-    void filter(I zuulMesg);
-    void filter(I zuulMesg, HttpContent chunk);
+    void filter(@Nullable I zuulMesg);
+
+    void filter(@Nullable I zuulMesg, HttpContent chunk);
 }
