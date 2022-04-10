0e5187e929a56603a180f569cca9315c06938ccb
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 04:55:11 -0700

    Final Result

--- zuul-core/src/main/java/com/netflix/zuul/exception/OutboundException.java
+++ zuul-core/src/main/java/com/netflix/zuul/exception/OutboundException.java
@@ -13,12 +13,11 @@
  *      See the License for the specific language governing permissions and
  *      limitations under the License.
  */
-
 package com.netflix.zuul.exception;
 
+import javax.annotation.Nullable;
 import com.netflix.zuul.niws.RequestAttempt;
 import com.netflix.zuul.niws.RequestAttempts;
-
 
 /**
  * Outbound Exception Decorator
@@ -27,13 +26,14 @@ import com.netflix.zuul.niws.RequestAttempts;
  * Date: 10/21/15
  * Time: 11:46 AM
  */
-public class OutboundException extends ZuulException
-{
+public class OutboundException extends ZuulException {
+
     private final ErrorType outboundErrorType;
+
+    @Nullable
     private final RequestAttempts requestAttempts;
 
-    public OutboundException(ErrorType outboundErrorType, RequestAttempts requestAttempts)
-    {
+    public OutboundException(ErrorType outboundErrorType, @Nullable RequestAttempts requestAttempts) {
         super(outboundErrorType.toString(), outboundErrorType.toString(), true);
         this.outboundErrorType = outboundErrorType;
         this.requestAttempts = requestAttempts;
@@ -41,8 +41,7 @@ public class OutboundException extends ZuulException
         this.dontLogAsError();
     }
 
-    public OutboundException(ErrorType outboundErrorType, RequestAttempts requestAttempts, Throwable cause)
-    {
+    public OutboundException(ErrorType outboundErrorType, @Nullable RequestAttempts requestAttempts, Throwable cause) {
         super(outboundErrorType.toString(), cause.getMessage(), true);
         this.outboundErrorType = outboundErrorType;
         this.requestAttempts = requestAttempts;
@@ -50,15 +49,12 @@ public class OutboundException extends ZuulException
         this.dontLogAsError();
     }
 
-    public RequestAttempt getFinalRequestAttempt()
-    {
+    @Nullable
+    public RequestAttempt getFinalRequestAttempt() {
         return requestAttempts == null ? null : requestAttempts.getFinalAttempt();
     }
 
-
-    public ErrorType getOutboundErrorType()
-    {
+    public ErrorType getOutboundErrorType() {
         return outboundErrorType;
     }
-
 }
