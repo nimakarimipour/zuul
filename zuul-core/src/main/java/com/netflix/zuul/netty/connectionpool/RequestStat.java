cbf9baffdb259764e84241c7e2a0df6d0bc32867
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-02 01:18:13 -0700

    fix: 20 - RETURN_NULLABLE - return super.get(key); - returning @Nullable expression from method with @NonNull return type

--- zuul-core/src/main/java/com/netflix/zuul/netty/connectionpool/RequestStat.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/connectionpool/RequestStat.java
@@ -19,6 +19,7 @@ package com.netflix.zuul.netty.connectionpool;
 import com.netflix.zuul.context.SessionContext;
 import com.netflix.zuul.discovery.DiscoveryResult;
 import com.netflix.zuul.exception.ErrorType;
+import javax.annotation.Nullable;
 
 /**
  * Request Stat
@@ -34,6 +35,7 @@ public interface RequestStat {
     return stat;
   }
 
+  @Nullable
   static RequestStat getFromSessionContext(SessionContext context) {
     return (RequestStat) context.get(SESSION_CONTEXT_KEY);
   }
