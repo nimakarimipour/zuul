9ccacc7004e857b8d104d2db8252ccc49bcdce9e
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-02 01:17:25 -0700

    Revert "fix: 13 - DEREFERENCE_NULLABLE - if (cause instanceof IllegalStateException && cause.getMessage().contains("server")) { - dereferenced expression cause.getMessage() is @Nullable"
    This reverts commit c1784493a26d7c9bb61625ce4c6b306b51b0b83f.

--- zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java
@@ -57,6 +57,8 @@ public class NettyRequestAttemptFactory {
 
     if (t instanceof Errors.NativeIoException
         && Errors.ERRNO_ECONNRESET_NEGATIVE == ((Errors.NativeIoException) t).expectedErr()) {
+      // This is a "Connection reset by peer" which we see fairly often happening when Origin
+      // servers are overloaded.
       LOG.warn("ERRNO_ECONNRESET_NEGATIVE mapped to RESET_CONNECTION", t);
       return RESET_CONNECTION;
     }
@@ -66,9 +68,7 @@ public class NettyRequestAttemptFactory {
     }
 
     final Throwable cause = t.getCause();
-    if (cause instanceof IllegalStateException
-        && NullabilityUtil.castToNonnull(cause.getMessage(), "cause isn't null")
-            .contains("server")) {
+    if (cause instanceof IllegalStateException && cause.getMessage().contains("server")) {
       LOG.warn("IllegalStateException mapped to NO_AVAILABLE_SERVERS", cause);
       return NO_AVAILABLE_SERVERS;
     }
