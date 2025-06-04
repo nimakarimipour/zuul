de298d32c05df7421c1a153a4a5c7c38529af59b
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-04 10:01:11 -0700

    Revert "fix: 3 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.netflix.zuul.context.SessionContextDecorator.decorate(com.netflix.zuul.context.SessionContext) returns @NonNull"
    This reverts commit cc07294163dd60f6cbb8dc06c590808f90cc29f0.

--- zuul-core/src/main/java/com/netflix/zuul/context/SessionContextDecorator.java
+++ zuul-core/src/main/java/com/netflix/zuul/context/SessionContextDecorator.java
@@ -15,10 +15,7 @@
  */
 package com.netflix.zuul.context;
 
-import javax.annotation.Nullable;
-
 /** User: michaels@netflix.com Date: 2/25/15 Time: 4:09 PM */
 public interface SessionContextDecorator {
-  @Nullable
   public SessionContext decorate(SessionContext ctx);
 }
