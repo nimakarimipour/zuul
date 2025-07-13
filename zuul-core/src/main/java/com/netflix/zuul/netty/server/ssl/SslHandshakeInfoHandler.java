157232610f4158332a031c8e85f04ed4e96769f9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:54:57 -0700

    fix: 2 - DEREFERENCE_NULLABLE - handshakeInfo.getProtocol().length() > 0 ? handshakeInfo.getProtocol() : "unknown"; - dereferenced expression handshakeInfo is @Nullable

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
@@ -25,6 +25,7 @@ import com.netflix.spectator.api.Registry;
 import com.netflix.zuul.netty.ChannelUtils;
 import com.netflix.zuul.passport.CurrentPassport;
 import com.netflix.zuul.passport.PassportState;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.ssl.ClientAuth;
@@ -202,13 +203,14 @@ public class SslHandshakeInfoHandler extends ChannelInboundHandlerAdapter {
       SslHandshakeCompletionEvent sslHandshakeCompletionEvent,
       @Nullable SslHandshakeInfo handshakeInfo) {
     if (spectatorRegistry == null) {
-      // May be null for testing.
       return;
     }
     try {
       if (sslHandshakeCompletionEvent.isSuccess()) {
         String proto =
-            handshakeInfo.getProtocol().length() > 0 ? handshakeInfo.getProtocol() : "unknown";
+            Nullability.castToNonnull(handshakeInfo.getProtocol()).length() > 0
+                ? handshakeInfo.getProtocol()
+                : "unknown";
         String ciphsuite =
             handshakeInfo.getCipherSuite().length() > 0
                 ? handshakeInfo.getCipherSuite()
