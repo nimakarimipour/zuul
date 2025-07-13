e6281728c9f60403dfc1f4fa25260a1f6d94e242
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 02:42:42 -0700

    before:14to13 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression t.getCause() is @Nullable' /home/nima/Developer/nullness-benchmarks/zuul/zuul-core/src/main/java/com/netflix/zuul/niws/RequestAttempt.java:299           exceptionType = t.getCause().getClass().getSimpleName();

--- zuul-core/src/main/java/com/netflix/zuul/niws/RequestAttempt.java
+++ zuul-core/src/main/java/com/netflix/zuul/niws/RequestAttempt.java
@@ -31,6 +31,7 @@ import com.netflix.zuul.netty.connectionpool.OriginConnectException;
 import io.netty.handler.timeout.ReadTimeoutException;
 import javax.annotation.Nullable;
 import javax.net.ssl.SSLHandshakeException;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /** User: michaels@netflix.com Date: 9/2/14 Time: 2:52 PM */
 public class RequestAttempt {
@@ -282,38 +283,38 @@ public class RequestAttempt {
   }
 
   public void setException(Throwable t) {
-    if (t != null) {
-      if (t instanceof ReadTimeoutException) {
-        error = "READ_TIMEOUT";
-        exceptionType = t.getClass().getSimpleName();
-      } else if (t instanceof OriginConnectException) {
-        OriginConnectException oce = (OriginConnectException) t;
-        if (oce.getErrorType() != null) {
-          error = oce.getErrorType().toString();
-        } else {
-          error = "ORIGIN_CONNECT_ERROR";
-        }
-
-        final Throwable cause = t.getCause();
-        if (cause != null) {
-          exceptionType = t.getCause().getClass().getSimpleName();
-        } else {
+      if (t != null) {
+        if (t instanceof ReadTimeoutException) {
+          error = "READ_TIMEOUT";
           exceptionType = t.getClass().getSimpleName();
+        } else if (t instanceof OriginConnectException) {
+          OriginConnectException oce = (OriginConnectException) t;
+          if (oce.getErrorType() != null) {
+            error = oce.getErrorType().toString();
+          } else {
+            error = "ORIGIN_CONNECT_ERROR";
+          }
+  
+          final Throwable cause = t.getCause();
+          if (cause != null) {
+            exceptionType = Nullability.castToNonnull(t.getCause(), "cause checked nonnull").getClass().getSimpleName();
+          } else {
+            exceptionType = t.getClass().getSimpleName();
+          }
+        } else if (t instanceof OutboundException) {
+          OutboundException obe = (OutboundException) t;
+          error = obe.getOutboundErrorType().toString();
+          exceptionType = OutboundException.class.getSimpleName();
+        } else if (t instanceof SSLHandshakeException) {
+          error = t.getMessage();
+          exceptionType = t.getClass().getSimpleName();
+          cause = t.getCause().getMessage();
+        } else {
+          error = t.getMessage();
+          exceptionType = t.getClass().getSimpleName();
+          cause = Throwables.getStackTraceAsString(t);
         }
-      } else if (t instanceof OutboundException) {
-        OutboundException obe = (OutboundException) t;
-        error = obe.getOutboundErrorType().toString();
-        exceptionType = OutboundException.class.getSimpleName();
-      } else if (t instanceof SSLHandshakeException) {
-        error = t.getMessage();
-        exceptionType = t.getClass().getSimpleName();
-        cause = t.getCause().getMessage();
-      } else {
-        error = t.getMessage();
-        exceptionType = t.getClass().getSimpleName();
-        cause = Throwables.getStackTraceAsString(t);
       }
-    }
   }
 
   public void setMaxRetries(int maxRetries) {
