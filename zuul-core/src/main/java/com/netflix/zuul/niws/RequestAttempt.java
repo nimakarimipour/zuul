26466e64d70bf9d67dc5e2682b621cdaf58d9cb6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:56:50 -0700

    Revert "fix: 15 - DEREFERENCE_NULLABLE - exceptionType = t.getCause().getClass().getSimpleName(); - dereferenced expression t.getCause() is @Nullable"
    This reverts commit 41058cb48b943694a13a0e0ccc02d6a3e4cb1897.

--- zuul-core/src/main/java/com/netflix/zuul/niws/RequestAttempt.java
+++ zuul-core/src/main/java/com/netflix/zuul/niws/RequestAttempt.java
@@ -28,7 +28,6 @@ import com.netflix.zuul.discovery.DiscoveryResult;
 import com.netflix.zuul.discovery.SimpleMetaInfo;
 import com.netflix.zuul.exception.OutboundException;
 import com.netflix.zuul.netty.connectionpool.OriginConnectException;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import io.netty.handler.timeout.ReadTimeoutException;
 import javax.annotation.Nullable;
 import javax.net.ssl.SSLHandshakeException;
@@ -297,10 +296,7 @@ public class RequestAttempt {
 
         final Throwable cause = t.getCause();
         if (cause != null) {
-          exceptionType =
-              Nullability.castToNonnull(t.getCause(), "cause checked nonnull")
-                  .getClass()
-                  .getSimpleName();
+          exceptionType = t.getCause().getClass().getSimpleName();
         } else {
           exceptionType = t.getClass().getSimpleName();
         }
