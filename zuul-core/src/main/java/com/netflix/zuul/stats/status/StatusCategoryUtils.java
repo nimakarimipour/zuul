7546c93ec6ba8fc2477c66b86ba62f4daba9c24f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:53:45 -0700

    fix: 17 - DEREFERENCE_NULLABLE - return ctx.get(CommonContextKeys.STATUS_CATGEORY); - dereferenced expression ctx is @Nullable

--- zuul-core/src/main/java/com/netflix/zuul/stats/status/StatusCategoryUtils.java
+++ zuul-core/src/main/java/com/netflix/zuul/stats/status/StatusCategoryUtils.java
@@ -33,6 +33,9 @@ public class StatusCategoryUtils {
   }
 
   public static StatusCategory getStatusCategory(@Nullable SessionContext ctx) {
+    if (ctx == null) {
+      return null; // Or handle this situation accordingly
+    }
     return ctx.get(CommonContextKeys.STATUS_CATGEORY);
   }
 
