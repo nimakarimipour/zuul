f48163a78eb115fabae320a76619dc3e380abac5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:52:50 -0700

    Revert "fix: 8 - SWITCH_EXPRESSION_NULLABLE - switch (state) { - switch expression state is @Nullable"
    This reverts commit 95473d1c3b506fe2dda434c90b64a79fb705761d.

--- zuul-core/src/main/java/com/netflix/zuul/passport/CurrentPassport.java
+++ zuul-core/src/main/java/com/netflix/zuul/passport/CurrentPassport.java
@@ -478,10 +478,6 @@ class CountingCurrentPassport extends CurrentPassport {
   }
 
   private void incrementStateCounter(@Nullable PassportState state) {
-    if (state == null) {
-      return;
-    }
-
     switch (state) {
       case IN_REQ_HEADERS_RECEIVED:
         IN_REQ_HEADERS_RECEIVED_CNT.increment();
