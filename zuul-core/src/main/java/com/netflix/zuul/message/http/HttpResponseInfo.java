64c568ae85f1b007aa92e2ed66cbe581d29640b9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:55:30 -0700

    Revert "fix: 6 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.netflix.zuul.message.http.HttpResponseInfo.getInboundRequest() returns @NonNull"
    This reverts commit dd056638edbe463aa1192bd269fe0bf9641e19ba.

--- zuul-core/src/main/java/com/netflix/zuul/message/http/HttpResponseInfo.java
+++ zuul-core/src/main/java/com/netflix/zuul/message/http/HttpResponseInfo.java
@@ -17,14 +17,12 @@
 package com.netflix.zuul.message.http;
 
 import com.netflix.zuul.message.ZuulMessage;
-import javax.annotation.Nullable;
 
 /** User: michaels@netflix.com Date: 7/6/15 Time: 5:27 PM */
 public interface HttpResponseInfo extends ZuulMessage {
   int getStatus();
 
   /** The immutable request that was originally received from client. */
-  @Nullable
   HttpRequestInfo getInboundRequest();
 
   @Override
