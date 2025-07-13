0c0d78a45e79c5ed6bd6808997594a9a5fd47a1f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:56:26 -0700

    Revert "fix: 12 - UNBOX_NULLABLE - computedTimeout = originTimeout == null ? requestTimeout : originTimeout; - unboxing of a @Nullable value - originTimeout == null ? requestTimeout : originTimeout"
    This reverts commit a02bd9ca3226da2b35ed8de9f0e837de1a05add9.

--- zuul-core/src/main/java/com/netflix/zuul/netty/timeouts/OriginTimeoutManager.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/timeouts/OriginTimeoutManager.java
@@ -24,7 +24,6 @@ import com.netflix.config.DynamicLongProperty;
 import com.netflix.zuul.context.CommonContextKeys;
 import com.netflix.zuul.message.http.HttpRequestMessage;
 import com.netflix.zuul.origins.NettyOrigin;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.time.Duration;
 import java.util.Objects;
 import java.util.Optional;
@@ -69,12 +68,13 @@ public class OriginTimeoutManager {
     if (originTimeout == null && requestTimeout == null) {
       computedTimeout = MAX_OUTBOUND_READ_TIMEOUT_MS.get();
     } else if (originTimeout == null || requestTimeout == null) {
-      computedTimeout =
-          Nullability.castToNonnull(originTimeout == null ? requestTimeout : originTimeout);
+      computedTimeout = originTimeout == null ? requestTimeout : originTimeout;
     } else {
+      // return the stricter (i.e. lower) of the two timeouts
       computedTimeout = Math.min(originTimeout, requestTimeout);
     }
 
+    // enforce max timeout upperbound
     return Duration.ofMillis(Math.min(computedTimeout, MAX_OUTBOUND_READ_TIMEOUT_MS.get()));
   }
 
