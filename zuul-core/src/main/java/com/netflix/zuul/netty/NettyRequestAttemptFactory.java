e9a6e81dd77c4b9cabacc3d28854de9f9ca64b25
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:48:10 -0700

    before:17to16 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression cause.getMessage() is @Nullable' /home/nima/Developer/nullness-benchmarks/zuul/zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java:71     if (cause instanceof IllegalStateException && cause.getMessage().contains("server")) {

--- zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java
@@ -39,42 +39,43 @@ public class NettyRequestAttemptFactory {
   private static final Logger LOG = LoggerFactory.getLogger(NettyRequestAttemptFactory.class);
 
   public ErrorType mapNettyToOutboundErrorType(final Throwable t) {
-    if (t instanceof ReadTimeoutException) {
-      return READ_TIMEOUT;
+      if (t instanceof ReadTimeoutException) {
+        return READ_TIMEOUT;
+      }
+  
+      if (t instanceof OriginConcurrencyExceededException) {
+        return ORIGIN_CONCURRENCY_EXCEEDED;
+      }
+  
+      if (t instanceof OriginConnectException) {
+        return ((OriginConnectException) t).getErrorType();
+      }
+  
+      if (t instanceof OutboundException) {
+        return ((OutboundException) t).getOutboundErrorType();
+      }
+  
+      if (t instanceof Errors.NativeIoException
+          && Errors.ERRNO_ECONNRESET_NEGATIVE == ((Errors.NativeIoException) t).expectedErr()) {
+        LOG.warn("ERRNO_ECONNRESET_NEGATIVE mapped to RESET_CONNECTION", t);
+        return RESET_CONNECTION;
+      }
+  
+      if (t instanceof ClosedChannelException) {
+        return RESET_CONNECTION;
+      }
+  
+      final Throwable cause = t.getCause();
+      if (cause instanceof IllegalStateException) {
+        String message = cause.getMessage();
+        if (message != null && message.contains("server")) {
+          LOG.warn("IllegalStateException mapped to NO_AVAILABLE_SERVERS", cause);
+          return NO_AVAILABLE_SERVERS;
+        }
+      }
+  
+      return OTHER;
     }
-
-    if (t instanceof OriginConcurrencyExceededException) {
-      return ORIGIN_CONCURRENCY_EXCEEDED;
-    }
-
-    if (t instanceof OriginConnectException) {
-      return ((OriginConnectException) t).getErrorType();
-    }
-
-    if (t instanceof OutboundException) {
-      return ((OutboundException) t).getOutboundErrorType();
-    }
-
-    if (t instanceof Errors.NativeIoException
-        && Errors.ERRNO_ECONNRESET_NEGATIVE == ((Errors.NativeIoException) t).expectedErr()) {
-      // This is a "Connection reset by peer" which we see fairly often happening when Origin
-      // servers are overloaded.
-      LOG.warn("ERRNO_ECONNRESET_NEGATIVE mapped to RESET_CONNECTION", t);
-      return RESET_CONNECTION;
-    }
-
-    if (t instanceof ClosedChannelException) {
-      return RESET_CONNECTION;
-    }
-
-    final Throwable cause = t.getCause();
-    if (cause instanceof IllegalStateException && cause.getMessage().contains("server")) {
-      LOG.warn("IllegalStateException mapped to NO_AVAILABLE_SERVERS", cause);
-      return NO_AVAILABLE_SERVERS;
-    }
-
-    return OTHER;
-  }
 
   public OutboundException mapNettyToOutboundException(
       final Throwable t, final SessionContext context) {
