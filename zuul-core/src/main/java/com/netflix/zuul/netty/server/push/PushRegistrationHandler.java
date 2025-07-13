4dcd48fc94283c40a497d16663f27b458a1fe5e4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:55:12 -0700

    fix: 4 - METHOD_NO_INIT - public PushRegistrationHandler( - initializer method does not guarantee @NonNull field ctx (line 44) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java
@@ -16,6 +16,7 @@ package com.netflix.zuul.netty.server.push;
 import com.google.common.annotations.VisibleForTesting;
 import com.netflix.config.CachedDynamicBooleanProperty;
 import com.netflix.config.CachedDynamicIntProperty;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
@@ -41,7 +42,7 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
 
   /* state */
   protected final AtomicBoolean destroyed;
-  private ChannelHandlerContext ctx;
+  @Nullable private ChannelHandlerContext ctx;
   @Nullable private volatile PushConnection pushConnection;
   private final List<ScheduledFuture<?>> scheduledFutures;
 
@@ -103,8 +104,8 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
     ctx.close();
   }
 
-  protected final void forceCloseConnectionFromServerSide() {
-    if (!destroyed.get()) {
+  protected final void forceCloseConnectionFromServerSide(ChannelHandlerContext ctx) {
+    if (!destroyed.get() && ctx != null) {
       logger.debug("server forcing close connection");
       pushProtocol.sendErrorAndClose(ctx, 1000, "Server closed connection");
     }
@@ -119,8 +120,8 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
     }
   }
 
-  private void requestClientToCloseConnection() {
-    if (ctx.channel().isActive()) {
+  private void requestClientToCloseConnection(ChannelHandlerContext ctx) {
+    if (ctx != null && ctx.channel().isActive()) {
       // Application level protocol for asking client to close connection
       ctx.writeAndFlush(pushProtocol.goAwayMessage());
       // Force close connection if client doesn't close in reasonable time after we made request
@@ -136,8 +137,8 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
   }
 
   protected void keepAlive() {
-    if (KEEP_ALIVE_ENABLED.get()) {
-      ctx.writeAndFlush(new PingWebSocketFrame());
+    if (ctx != null && KEEP_ALIVE_ENABLED.get()) {
+      Nullability.castToNonnull(ctx, "checked for nullity").writeAndFlush(new PingWebSocketFrame());
     }
   }
 
